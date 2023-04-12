6c5d5a5f9b8380404cc1b2dc27bdd7a2ec3477d4
bernardoboe <111295043+bernardoboe@users.noreply.github.com>
2023-04-11 21:55:41 -0300

    Criando classe Vendedor
    :)

---
+++ Ex1-POO/src/Vendedor.java
@@ -0,0 +1,20 @@
+
+public class Vendedor {
+
+	private double totalDasVendas;
+	private double comissao;
+	
+	public Vendedor(String nome, String cpf, double totalDasVendas, double comissao) {
+        super(nome, cpf);
+        
+        this.totalDasVendas = totalDasVendas;
+        this.comissao = comissao;
+    }
+
+    @Override
+    public double calcularSalario() {
+        return this.totalDasVendas * this.comissao;
+    }
+
+	
+}
