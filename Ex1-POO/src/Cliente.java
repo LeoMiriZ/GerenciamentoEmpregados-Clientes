6d0d58462b9a2087ca7a991faaa10d05976c4b19
DanSanJu <107129770+DanSanJu@users.noreply.github.com>
2023-04-11 21:52:39 -0300

    Criando classe Cliente

---
+++ Ex1-POO/src/Cliente.java
@@ -0,0 +1,49 @@
+
+public class Cliente extends Pessoa {
+	
+	private double valorDaDivida;
+
+	
+
+    public Cliente(String nome, String cpf, double valorDaDivida) {
+    
+    super(nome, cpf);
+
+        this.valorDaDivida = valorDaDivida;
+    }
+    
+    
+    
+    
+   
+    public double getValorDaDivida() {
+        
+    	return valorDaDivida;
+    }
+
+    
+    
+    
+    
+    public void setValorDaDivida(double valorDaDivida) {
+       
+    	this.valorDaDivida = valorDaDivida;
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
