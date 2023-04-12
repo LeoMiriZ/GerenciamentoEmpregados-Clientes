682d8901a31674950236cd61cf855539978c5051
bernardoboe <111295043+bernardoboe@users.noreply.github.com>
2023-04-11 23:18:23 -0300

    Incluindo mais métodos na classe

---
+++ Ex1-POO/src/Metodos.java
@@ -0,0 +1,102 @@
+
+	import static javax.swing.JOptionPane.showInputDialog;
+	import static javax.swing.JOptionPane.showMessageDialog;
+
+import java.util.ArrayList;
+
+	public class Metodos {
+
+		private ArrayList<Pessoa> pessoa = new ArrayList<>();  
+		
+		public void menu() {
+
+			int aux = 0;
+
+			while (true) {
+
+				aux = Integer.parseInt(showInputDialog(
+						"Escolha uma operação: \n1. Cadastrar Empregado \n2. Cadastrar Cliente \n3. Pesquisar \n4. Listar Empregados \n 5. Listar Clientes \n6. Remover \n7. Finalizar"));
+
+				if (aux == 1) {
+
+					cadastrarEmpregado();
+
+				} else if (aux == 2) {
+
+					cadastrarCliente();
+
+				} else if (aux == 3) {
+
+					pesquisar();
+
+				} else if (aux == 4) {
+
+					listarEmpregados();
+
+				} else if(aux == 5) {
+					
+					listarClientes();
+					
+				} else if (aux == 6) {
+					
+					remover();
+					
+				} else if (aux == 7) {
+					
+					showMessageDialog(null, "Operação finalizada com sucesso!");
+					break;
+					
+				} else {
+					
+					showMessageDialog(null, "Insira uma operação válida");
+					return;
+					
+				}
+
+			}
+
+		}
+		
+		public void cadastrarEmpregado() {
+			int aux = 0;
+			String nome, cpf, matricula;
+			
+			nome = showInputDialog("Insira seu nome");
+			cpf = showInputDialog("Insira seu CPF");
+			
+			while(aux != 1 && aux != 2) {
+			aux = Integer.parseInt(showInputDialog("Digite 1 para Vendedor ou 2 para Gerente."));
+			
+			if(aux == 1) {
+				
+				
+				
+			}else if (aux == 2) {
+				
+			}else {
+				showMessageDialog(null, "Digite um valor valido.");
+			}
+			}
+		}
+		
+		public void cadastrarCliente() {
+			
+		}
+		
+		public void pesquisar() {
+			
+		}
+
+		public void listarEmpregados() {
+			
+		}
+		
+		public void listarClientes() {
+			
+		}
+		
+		public void remover() {
+			
+		}
+	}
+
